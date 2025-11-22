93845600c808a4e7d319e924f90822842e32aba9
Will <129187264+WYll1734@users.noreply.github.com>
2025-11-22 19:56:10 +1000

    Update page.js

--- app/dashboard/[guildId]/moderation/page.js
+++ app/dashboard/[guildId]/moderation/page.js
@@ -1,5 +1,9 @@
 import AutomodPage from "./automod/page";
 
 export default function ModerationIndexPage({ params }) {
-  return <AutomodPage params={params} />;
+  // extract correct param name
+  const guildId = params.guildId;
+
+  // Forward correct param shape to the subpage
+  return <AutomodPage params={{ guildId }} />;
 }
